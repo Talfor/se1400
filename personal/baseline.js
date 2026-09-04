cb1b99d64b06090139ae375f5d38fae6f5920615
Talfor <kyle.bennett.1914@gmail.com>
2026-09-03 19:03:01 -0600

    Milestone 2

---
+++ personal/baseline.js
@@ -0,0 +1,20 @@
+function name() {
+    alert("Test Message");
+}
+
+/*
+
+------------- Java Script Handbook -------------
+
+VARIABLETYPE NAME = VARIABLE;
+    VARIABLETYPES:
+        const = constant
+        let = open variable
+
+console.COMMAND(INPUT);
+    COMMANDS:
+        info("Text") = print info message to console
+        warn("Text") = print warning message to console
+        error("Text") = print error message to console
+
+*/
